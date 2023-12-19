6fa35cc34c4fe0cd7b9cfd4bb8fc872f50d9af4f
David <65980127+dyang5@users.noreply.github.com>
2023-12-18 23:46:26 -0500

    Lots of progress. Currently working on setting order to make vertices remain on top.

--- circularity_visualizer.py
+++ circularity_visualizer.py
@@ -16,26 +16,13 @@ class PolygonDrawer:
         self.current_polygon_x = []
         self.current_polygon_y = []
         self.current_polygon_vertices = []
-        
+        self.undo_stack = []
+
         self.ax.set_title("Circularity of Partitions Visualizer")
         self.ax.set_xlim(-1, 11)
         self.ax.set_ylim(-1, 11)
-        self.vertices.append([(0, 0), [0, 10], [10, 0], [10, 10]])
-        self.ax.plot(0, 0, 'ro')
-        self.ax.plot(0, 10, 'ro')
-        self.ax.plot(10, 0, 'ro')
-        self.ax.plot(10, 10, 'ro')
-
-        self.fig.gca().set_aspect('equal')
-
-
-        self.ax.set_axis_off()
-        self.patches.append(mpl.patches.Rectangle((0, 0),10,10))
-        self.ax.add_collection(PatchCollection(self.patches, facecolors='w', edgecolors='black'))
-
         self.fig.canvas.mpl_connect('button_press_event', self.on_click)
         self.fig.canvas.mpl_connect('key_press_event', self.on_key)
-
 
     def on_click(self, event):
         # if in adding vertices stage and left mouse button is clicked
@@ -72,29 +59,52 @@ class PolygonDrawer:
                     print("Need at least 3 vertices.")
                 else:
                     self.create_polygon(self.current_polygon_vertices)
+                    self.current_polygon_vertices = [ ]
 
 
         elif not self.adding_vertices and event.button == 3:  # Right mouse button
             self.finish_polygon()
 
     def on_key(self, event):
+        # vertex addition phase
         if event.key == 'v':
             self.adding_vertices = not self.adding_vertices    
             if self.adding_vertices:
                 print("Adding vertices. Click to add a vertex.")
             else:
                 print("Adding convex polygon partitions.")
+
+        # set up phase
+        elif event.key == 't':
+            self.setup()
+
+    def setup(self):
+        vertex_coordinates = [(0, 0), [0, 10], [10, 0], [10, 10]]
+        self.vertices.append(vertex_coordinates)
+
+        for coord in vertex_coordinates:
+            x, y = coord
+            vertex = mpl.patches.Circle((x, y), radius = 0.1, color = 'red')
+            vertex.center = coord
+            self.vertex_patches.append(vertex)
+
+            self.patches.append(vertex)
+            self.ax.add_patch(self.patches[len(self.patches) - 1])
+            self.fig.canvas.draw()
+        self.fig.gca().set_aspect('equal')
+
+        self.ax.set_axis_off()
+        self.patches.append(mpl.patches.Rectangle((0, 0),10,10))
+        self.ax.add_collection(PatchCollection(self.patches, facecolors='w', edgecolors='black'))        
+        self.fig.canvas.draw()
     
     def create_polygon(self, coordinateLst):
         self.partitions.append(Polygon(coordinateLst, closed=True, facecolor = 'white', edgecolor='black'))
+        self.patches.append(self.partitions[len(self.partitions) - 1])
         self.ax.add_patch(self.partitions[len(self.partitions) - 1])
         self.fig.canvas.draw()
+        self.save_state()
         
-    def start_polygon(self, x, y):
-        self.polygon = [((x, y))]
-        self.patches = [Polygon(self.polygon, closed=False, edgecolor='r')]
-        self.ax.add_collection(PatchCollection(self.patches, facecolors='b', edgecolors='r'))
-        self.fig.canvas.draw()
 
     def add_point(self, x, y):
         self.vertices.append((x, y))
@@ -103,17 +113,47 @@ class PolygonDrawer:
         self.vertex_patches.append(vertex)
 
         self.patches.append(vertex)
-        self.ax.add_patch(self.patches[len(self.patches) - 1])
+        self.ax.add_patch(self.patches[len(self.patches) - 1]) # (zorder gives priority to patches)
         self.fig.canvas.draw()
+        self.save_state()
 
-    def finish_polygon(self):
-        if self.polygon is not None:
-            self.polygon.append(self.polygon[0])  # Close the polygon
-            self.patches[0] = Polygon(self.polygon, closed=True, edgecolor='r')
-            self.ax.add_collection(PatchCollection(self.patches, facecolors='b', edgecolors='r'))
+
+    # def start_polygon(self, x, y):
+    #     self.polygon = [((x, y))]
+    #     self.patches = [Polygon(self.polygon, closed=False, edgecolor='r')]
+    #     self.ax.add_collection(PatchCollection(self.patches, facecolors='b', edgecolors='r'))
+    #     self.fig.canvas.draw()
+
+    # def finish_polygon(self):
+    #     if self.polygon is not None:
+    #         self.polygon.append(self.polygon[0])
+    #         self.patches[0] = Polygon(self.polygon, closed=True, edgecolor='r')
+    #         self.ax.add_collection(PatchCollection(self.patches, facecolors='b', edgecolors='r'))
+    #         self.fig.canvas.draw()
+    #         self.polygon = None
+
+    # keep track of states in stack
+    def save_state(self):
+        self.undo_stack.append((self.vertices.copy(), self.partitions.copy()))
+
+    def undo(self):
+        if self.undo_stack:
+            self.vertices, self.partitions = self.undo_stack.pop()
+            self.ax.cla()
+            self.ax.set_title("Circularity of Partitions Visualizer")
+            self.ax.set_xlim(-1, 11)
+            self.ax.set_ylim(-1, 11)
+
+            for vertex in self.vertex_patches:
+                self.ax.add_patch(vertex)
+
+            for partition in self.partitions:
+                self.ax.add_patch(partition)
+
             self.fig.canvas.draw()
-            self.polygon = None
-
+            print("Undo complete.")
+        else:
+            print("Nothing to undo.")
 
 if __name__ == "__main__":
     drawer = PolygonDrawer()
--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpl_patches
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
 class PolygonDrawer:
     def __init__(self):
         self.fig, self.ax = plt.subplots()
-        self.vertices = []
+        self.partitions = []
         self.polygon = None
         self.patches = []
-        self.is_drawing_points = True
+        self.vertex_patches = []
+        self.adding_vertices = True
+        self.vertices = []
+        self.current_polygon_x = []
+        self.current_polygon_y = []
+        self.current_polygon_vertices = []
+        self.undo_stack = []
 
-        self.ax.set_title("Interactive Polygon Drawer")
+        self.ax.set_title("Circularity of Partitions Visualizer")
         self.ax.set_xlim(-1, 11)
         self.ax.set_ylim(-1, 11)
         self.vertices.append([(0, 0), [0, 10], [10, 0], [10, 10]])
@@ -19,46 +25,97 @@ class PolygonDrawer:
         self.ax.plot(0, 10, 'ro')
         self.ax.plot(10, 0, 'ro')
         self.ax.plot(10, 10, 'ro')
-        self.patches.append(mpl.patches.Rectangle((0, 0),10,10))
+
+        self.fig.gca().set_aspect('equal')
+
+        self.ax.set_axis_off()
+        self.patches.append(mpl_patches.Rectangle((0, 0), 10, 10))
         self.ax.add_collection(PatchCollection(self.patches, facecolors='w', edgecolors='black'))
-        self.fig.canvas.draw()
 
         self.fig.canvas.mpl_connect('button_press_event', self.on_click)
         self.fig.canvas.mpl_connect('key_press_event', self.on_key)
 
     def on_click(self, event):
-        if self.is_drawing_points and event.button == 1:  # Left mouse button
-            self.add_vertex(event.xdata, event.ydata)
-        elif not self.is_drawing_points and event.button == 1:  # Left mouse button
-            self.create_polygon()
+        if self.adding_vertices and event.button == 1:  # Left mouse button
+            print("GOOD " + str(event.xdata) + " " + str(event.ydata))
+            self.add_point(event.xdata, event.ydata)
+
+        elif not self.adding_vertices and event.button == 1:  # Left mouse button
+            self.current_polygon_x = []
+            self.current_polygon_y = []
+
+            x_coord, y_coord = event.xdata, event.ydata
+            is_contained = False
+
+            for vertex in self.vertex_patches:
+                # use transformed coordinates (coordinates are different once added to plot) -- debugging took 1 hour lol
+                if vertex.contains_point(self.ax.transData.transform((x_coord, y_coord))):
+                    is_contained = True
+                    self.current_polygon_vertices.append(vertex.center)
+                    break
+
+            if not is_contained:
+                print("Chosen point is not a valid vertex. Pick a valid vertex.")
+
+        elif not self.adding_vertices and event.button == 3:  # Right mouse button
+            self.create_polygon(self.current_polygon_vertices)
+            self.current_polygon_vertices = []
 
     def on_key(self, event):
-        if event.key == 't':
-            self.is_drawing_points = not self.is_drawing_points
-            if self.is_drawing_points:
-                print("Drawing points mode. Click to add vertices.")
+        if event.key == 'v':
+            self.adding_vertices = not self.adding_vertices
+            if self.adding_vertices:
+                print("Adding vertices. Click to add a vertex.")
             else:
-                print("Polygon creation mode. Click to create a polygon.")
+                print("Adding convex polygon partitions.")
+        elif event.key == 'z':
+            self.undo()
 
-    def add_vertex(self, x, y):
-        self.vertices.append((x, y))
-        self.ax.plot(x, y, 'ro')  # Mark the vertex with a red dot
-        self.fig.canvas.draw()
-
-    def create_polygon(self):
-        if len(self.vertices) >= 3:
-            self.vertices.append(self.vertices[0])  # Close the polygon
-            self.polygon = Polygon(self.vertices, closed=True, edgecolor='r')
-            self.patches = [self.polygon]
-            self.ax.patches = []
-            self.ax.add_collection(PatchCollection(self.patches, facecolors='b', edgecolors='r'))
+    def create_polygon(self, coordinate_lst):
+        if coordinate_lst:
+            polygon = Polygon(coordinate_lst, closed=True, facecolor='white', edgecolor='black')
+            self.partitions.append(polygon)
+            self.patches.append(polygon)
+            self.ax.add_patch(polygon)
             self.fig.canvas.draw()
-            self.vertices = []
-            print("Polygon created.")
+            self.save_state()
+
+    def add_point(self, x, y):
+        self.vertices.append((x, y))
+        vertex = mpl_patches.Circle((x, y), radius=0.1, color='red')
+        vertex.center = (x, y)
+        self.vertex_patches.append(vertex)
+
+        self.patches.append(vertex)
+        self.ax.add_patch(vertex)
+        self.fig.canvas.draw()
+        self.save_state()
+
+    def save_state(self):
+        # Save the current state for undo
+        self.undo_stack.append((self.vertices.copy(), self.partitions.copy()))
+
+    def undo(self):
+        if self.undo_stack:
+            self.vertices, self.partitions = self.undo_stack.pop()
+            self.ax.cla()
+            self.ax.set_title("Circularity of Partitions Visualizer")
+            self.ax.set_xlim(-1, 11)
+            self.ax.set_ylim(-1, 11)
+
+            for vertex in self.vertex_patches:
+                self.ax.add_patch(vertex)
+
+            for partition in self.partitions:
+                self.ax.add_patch(partition)
+
+            self.fig.canvas.draw()
+            print("Undo complete.")
         else:
-            print("At least 3 vertices are required to create a polygon.")
+            print("Nothing to undo.")
+
 
 if __name__ == "__main__":
     drawer = PolygonDrawer()
-    print("Drawing points mode. Click to add vertices.")
+
     plt.show()
